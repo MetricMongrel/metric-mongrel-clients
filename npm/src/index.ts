f70a4cbed91a08a524bb814c331440dd09085ab4
Sid Premkumar <sid.premkumar@gmail.com>
2024-04-07 22:17:00 -0700

    Name export

--- npm/src/index.ts
+++ npm/src/index.ts
@@ -1,6 +1,6 @@
 /**
  * Main file with all exports from the Metric Mongrel NPM packages
  */
-export * from "./ErrorRequestHandler";
-export * from "./MMLogger";
-export * from "./MetricCollector";
+export * as ErrorRequestHandler from "./ErrorRequestHandler";
+export * as MMLogger from "./MMLogger";
+export * as MetricCollector from "./MetricCollector";
