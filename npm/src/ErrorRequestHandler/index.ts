c1fefdf9fe5cf58fbfbf21d5fa67911b623d7bea
Sid Premkumar <sid.premkumar@gmail.com>
2024-04-07 22:31:31 -0700

    rip file

--- npm/src/ErrorRequestHandler/index.ts
+++ npm/src/ErrorRequestHandler/index.ts
@@ -1,2 +1,46 @@
-import { ErrorRequestHandler } from "./ErrorRequestHandler";
-export { ErrorRequestHandler };
+import { NextFunction, Response, Request } from "express";
+import metricCollector from "../MetricCollector";
+import { MMLoggerMetadata } from "../MMLogger/MMLogger.types";
+import Logger from "../MMLogger";
+
+const ErrorRequestHandlerLogger = new Logger("error-request-handler");
+
+/**
+ * Log this error to our server, with the metadata of the
+ * request
+ */
+export function ErrorRequestHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  logger?: Logger
+) {
+  if (err) {
+    try {
+      /**
+       * Extract any metadata if present in the logger context
+       */
+      let loggerMetadata: undefined | MMLoggerMetadata = undefined;
+      if (logger) {
+        loggerMetadata = logger.getMetadata();
+      }
+
+      /**
+       * Report this back to HQ
+       * @note This is a un-awaited promise, and will not block the request
+       */
+      metricCollector.increment(
+        "error-request-handler",
+        1,
+        loggerMetadata,
+        "AUTO"
+      );
+    } catch (err) {
+      ErrorRequestHandlerLogger.error(`Error reporting error to HQ: ${err}`);
+    }
+  }
+  next(err);
+}
+
+export default ErrorRequestHandler;
--- npm/src/index.ts
+++ npm/src/index.ts
@@ -1,6 +1,6 @@
 /**
  * Main file with all exports from the Metric Mongrel NPM packages
  */
-export * as ErrorRequestHandler from "./ErrorRequestHandler";
-export * as MMLogger from "./MMLogger";
-export * as MetricCollector from "./MetricCollector";
+export * as ErrorRequestHandler from "./ErrorRequestHandler/index";
+export * as MMLogger from "./MMLogger/index";
+export * as MetricCollector from "./MetricCollector/index";
